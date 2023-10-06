6cfb1284d50dc23f9d6df6c0e8aacfa159205bca
Hang Phan <funnyluv122@gmail.com>
2023-10-06 16:49:12 -0500

    initial

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using HP.PersonalStocks.Mgr;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           // var mgr = new AlertMgr("MSFT");
+
+            // var suggest = mgr.GetSuggestionForCurrentSticker();
+
+            //var result = mgr.CheckForAlert();
+
+            var stockNaturalSelectionMgr = new StockNaturalSelectionMgr("AAPL");
+            await stockNaturalSelectionMgr.StartToCalculateAsync();
+        }
+    }
+}
