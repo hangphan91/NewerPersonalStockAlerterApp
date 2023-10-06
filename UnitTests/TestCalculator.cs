1e20e28ff96640481dbb119bbf20b2902a5968e4
Hang Phan <funnyluv122@gmail.com>
2023-10-06 16:49:49 -0500

    initial

---
+++ UnitTests/TestCalculator.cs
@@ -0,0 +1,23 @@
+﻿using HP.PersonalStocks.Mgr.Logics;
+using HP.PersonalStocksAlerter.Models.Models;
+
+namespace UnitTests;
+
+public class TestCalculator
+{
+    [Fact]
+    public void TestSetAvailableBalance()
+    {
+
+        var stockNatualSelectionCalculator = new StockNatualSelectionCalculator();
+        var monthlyDeposit = stockNatualSelectionCalculator.PositionLedgerSummary.DepositAmountMonthly;
+        stockNatualSelectionCalculator.PositionLedgerSummary.BalanceHolders.Add(new BalanceHolder
+        {
+            Amount = monthlyDeposit,
+            Date = DateTime.Now,
+        });
+        stockNatualSelectionCalculator.SetAvailableBalance();
+
+        Assert.Equal(stockNatualSelectionCalculator.PositionLedgerSummary.Statistics.AvalableBalance, monthlyDeposit);
+    }
+}
